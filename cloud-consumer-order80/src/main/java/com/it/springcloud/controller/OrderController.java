d9909f073d0525d014dba7d0a924204bc561af51
lidada <1633016576@qq.com>
2020-11-23 23:08:23 +0800

    cloud基础架构

--- cloud-consumer-order80/src/main/java/com/it/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/it/springcloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.it.springcloud.controller;
 
 import com.it.springcloud.entity.CommonResult;
 import com.it.springcloud.entity.Payment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,12 +19,23 @@ public class OrderController {
     private RestTemplate restTemplate;
 
     @GetMapping("/consumer/payment/create")
-    public CommonResult create(Payment payment){
+    public CommonResult<Payment> create(Payment payment){
         return restTemplate.postForObject(PROVIDER_URL+"/payment/create",payment,CommonResult.class);
     }
 
     @GetMapping("/consumer/payment/getId/{id}")
-    public CommonResult getId(@PathVariable("id") Long id){
+    public CommonResult<Payment> getId(@PathVariable("id") Long id){
         return restTemplate.getForObject(PROVIDER_URL+"/payment/getId/"+id,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getId2/{id}")
+    public CommonResult getId2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(PROVIDER_URL + "/payment/getId/" + id, CommonResult.class);
+        if(forEntity.getStatusCode().is2xxSuccessful()) {
+            return forEntity.getBody();
+        }else{
+            return new CommonResult(444,"查询失败");
+        }
+
     }
 }
