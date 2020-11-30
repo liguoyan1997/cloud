ea289f56b81a32249b325b2ce1b851c17067b6e9
lidada <1633016576@qq.com>
2020-11-30 22:08:39 +0800

    cloud基础架构之config配置

---
+++ cloud-config-client-3355/src/main/java/com/it/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,22 @@
+package com.it.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 动态刷新配置@RefreshScope
+ */
+@RefreshScope
+@RestController
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
